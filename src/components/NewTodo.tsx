e33a1335dfb13ab41fa9d156aafe4e199ecbeae7
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-04 21:41:07 +0300

    Retrieving entered todo text was implemented

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,23 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = textInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error...
+      return;
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor='text'>Todo text</label>
-      <input id='text' type='text' />
+      <input id='text' type='text' ref={textInputRef} />
       <button>Add Todo</button>
     </form>
   );
